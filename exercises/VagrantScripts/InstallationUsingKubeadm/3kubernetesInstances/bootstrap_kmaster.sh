b2aab613416d1d40402b1b77e92acc3bc4dc1c2c
vilasvarghese <vilas.varghese@gmail.com>
2020-10-25 09:58:47 +0530

    add changes

---
+++ exercises/VagrantScripts/InstallationUsingKubeadm/3kubernetesInstances/bootstrap_kmaster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize Kubernetes
+echo "Simple template doing nothing"
+
+#Install ssh so that we can login using putty
+echo "[TASK 5] Install ssh so that we can access using putty"
+yum –y install openssh-server openssh-clients
+systemctl start sshd
+systemctl status sshd
+systemctl enable sshd
